8aadc02c51ac71c43368d08913b21ae04ad1ec25
Nisha Mane <nishamane2104@gmail.com>
2026-03-28 23:54:09 +0530

    lesson3

---
+++ Desktop/Biopython/lesson3.py
@@ -0,0 +1,17 @@
+name = ("Nisha")
+fav_organism = ("Drosophila melanogaster")
+print(name)
+print(fav_organism)
+
+var_1 = 5
+var_2 = 20
+print (var_1)
+print(var_2)
+print (var_1 + var_2)
+print (var_1 - var_2)
+
+a = 18
+b = 26
+c = 36
+average = (a + b + c)/3
+print(average)
