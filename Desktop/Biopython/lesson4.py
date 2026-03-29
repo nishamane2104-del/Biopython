b89ff080c10aede44f28855bd942605544ab4a75
Nisha Mane <nishamane2104@gmail.com>
2026-03-29 23:33:07 +0530

    lesson4

---
+++ Desktop/Biopython/lesson4.py
@@ -0,0 +1,35 @@
+# Question 1 : Area of circle
+Radius = 7
+# Value of pi = 3.14
+# Area of circle = pi * radius^2
+pi = 3.14
+area = pi * Radius**2
+print ("Area of circle is", area)
+
+# Question 2 : Simple Interest
+# Given:
+# Principle (P): 5000
+# Rate (R): 5
+# Time(T): 2 years
+# Formula:
+# Simple Interest = (P * R * T) / 100
+P = 5000
+R = 5
+T = 2
+Simple_Interest = (P * R * T) / 100
+print("Simple Interest is", Simple_Interest)
+
+# Question 3 : Daily Water Intake
+# Problem statement: 
+# A person drinks water at different times of the day. 
+# The amount of water consumed at each time is as follows:
+# Given 
+# Mrng : 2 glasses 
+# Afternoon : 3 glasses
+# Evening : 1 glass
+
+M = 2
+A = 3
+E = 1
+Total_Water_Intake = M + A + E
+print("Total water intake in a day is", Total_Water_Intake, "glasses")
