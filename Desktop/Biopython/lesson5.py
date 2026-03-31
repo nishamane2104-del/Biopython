1c000b1cef8b18bcac18288998b84faef2e6b236
Nisha Mane <nishamane2104@gmail.com>
2026-03-31 15:56:20 +0530

    lesson 5

---
+++ Desktop/Biopython/lesson5.py
@@ -0,0 +1,29 @@
+dna = "ATGCGT"
+print(dna)
+print(type(dna)) 
+print(len(dna))
+
+# lenght of a DNA sequence 
+"""
+ "ATGCGTACGTAGC"
+Print the total number of nucleotides in the above sequence
+"""
+dna = "ATGCGTACGTAGC"
+length = len(dna)
+print("The total number of nucleotides in the sequence is", length)
+#Question 2: Accessing specific nucleotides in a DNA sequence
+"""
+Given the DNA seq:
+
+"AGCTAGCTAGCTAGC"
+
+Write:
+1. the first nucleotide
+2. the third nucleotide
+3. the last nucleotide
+"""
+dna = "AGCTAGCTAGCTAGC"
+print("The first nucleotide is", dna[0])
+print("The third nucleotide is", dna[2])
+print("The last nucleotide is", dna[-1])
+
