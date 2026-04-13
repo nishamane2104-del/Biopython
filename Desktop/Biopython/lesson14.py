0799facf1f336c0444f5a099a1b4b0d0158a62b2
Nisha Mane <nishamane2104@gmail.com>
2026-04-13 23:22:43 +0530

    how to read blast.xml file

--- Desktop/Biopython/lesson14.py
+++ Desktop/Biopython/lesson14.py
@@ -5,3 +5,16 @@ with open("blast_result.xml") as b:
     blast_record = NCBIXML.read(b) 
 
 print(len(blast_record.alignments))
+
+first_alignment = blast_record.alignments[0]
+# print("Title:", first_alignment.title)
+# print("Length:", first_alignment.length)
+
+# for a in blast_record.alignments:
+#     print(a.title)
+   
+print(len(first_alignment.hsps))
+
+first_hsp = first_alignment.hsps[0]
+print(first_hsp.score)
+print(first_hsp.expect)
